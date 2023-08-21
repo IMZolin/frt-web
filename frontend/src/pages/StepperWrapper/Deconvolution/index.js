025ade06ab2c90219e9d7b2ae2026ab47d851190
Sachuk A <alex.sachuk@yandex.ru>
2023-08-21 23:41:25 +0300

    Implemented frontend of part deconvolution stepper

--- frontend/src/pages/StepperWrapper/Deconvolution/index.js
+++ frontend/src/pages/StepperWrapper/Deconvolution/index.js
@@ -4,6 +4,7 @@ import StepperWrapper from '..';
 import Dropzone from '../../../components/Dropzone';
 import TifCompare from '../../../components/TifCompare';
 import TiffStackViewer from '../../../components/TiffStackViewer';
+import FileDownloader from '../../../components/FileDownloader';
 import { useStateValues } from "../state";
 import ChooseList from '../../../components/ChooseList';
 
@@ -11,16 +12,57 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Deconvolution = () => {
   const state = useStateValues();
-  const steps = ['Load image', 'Load PSF', 'Run Deconvolution', 'Save results'];
+  const steps = ['Load PSF', 'Load image', 'Run Deconvolution', 'Save results'];
+
+  const handleDeconvolve = async () => {
+    console.log("Im tryin make deconvolve");
+    
+    // TODO : need here to provide deconvolution and saving result in 2 variables: 'resultImage' (for preview) and 'resultImageSave' (for saving in file). It is like in PSF index.js
+    state.setResultImage(state.sourceImage);
+    console.log(state.sourceImage)
+    state.setResultImageSave(state.sourceImage);   
+  };
 
   function getStepContent(step) {
     switch (step) {
       case 0:
-        return (
-          <>
-            <Dropzone files={state.extractBeads} addFiles={state.setExtractBeads} imageType={'extract_beads'} state={state}/>
-          </>
-        );
+        return (<>
+                  <div className="row">
+                    <div className="column-1" style={{ zIndex: 2 }}>
+                      <div className="subtitle">Voxel size:</div>
+                      <div className="voxel-box">
+                        <TextField
+                          className="stepper-resolution"
+                          id="resolution-x"
+                          label="Resolution-XY (micron/pxl)"
+                          variant="outlined"
+                          placeholder="Enter the resolution in X and Y direction"
+                          fullWidth
+                          margin="normal"
+                          onChange={(e) => {
+                            state.setVoxelX(e.target.value);
+                            state.setVoxelY(e.target.value)
+                          }}
+                          value={state.voxelX}
+                        />
+                        <TextField
+                          className="stepper-resolution"
+                          id="resolution-z"
+                          label="Resolution-Z (micron/pxl)"
+                          variant="outlined"
+                          placeholder="Enter the resolution in Z direction"
+                          fullWidth
+                          margin="normal"
+                          onChange={(e) => state.setVoxelZ(e.target.value)}
+                          value={state.voxelZ}
+                        />
+                      </div>
+                    </div>
+                    <div className="column-2" style={{ zIndex: 1 }}>
+                      <Dropzone files={state.extractedPSF} addFiles={state.setExtractedPSF} imageType={'extracted_PSF'} state={state} />
+                    </div>
+                  </div>
+                </>);
       case 1:
         return (
           <>
@@ -28,13 +70,21 @@ const Deconvolution = () => {
               <div className="column-1">
                 <div className="slider-container">
                   <label htmlFor="scale-slider">Scale:</label>
-                  <input id="scale-slider" type="range" min="0.5" max="10" step="0.1" value={state.scale} onChange={state.handleSliderChange} />
+                  <input
+                    id="scale-slider"
+                    type="range"
+                    min="0.5"
+                    max="10"
+                    step="0.1"
+                    value={state.scale}
+                    onChange={(e) => state.handleScaleChange(e, 10)}
+                  />
                 </div>
-                <Dropzone files={state.psfFiles} addFiles={state.addPsfFiles} imageType={'psf_image'} state={state}/>
+                <Dropzone files={state.sourceImage} addFiles={state.setSourceImage} imageType={'source_img'} state={state}/>
               </div>
               <div className="column-2">
                 <div className="images__preview">
-                  <TifCompare files_1={state.extractBeads} files_2={state.psfFiles} scale={state.scale} state={state} canvasRef={null} isExtract={false}/>
+                  <TifCompare img_1={state.extractedPSF} img_2={state.sourceImage} scale={state.scale} state={state}/>
                 </div>
               </div>
             </div>
@@ -47,7 +97,15 @@ const Deconvolution = () => {
               <div className="column-1">
                 <div className="slider-container">
                   <label htmlFor="scale-slider">Scale:</label>
-                  <input id="scale-slider" type="range" min="0.5" max="10" step="0.1" value={state.scale} onChange={state.handleSliderChange} />
+                  <input
+                    id="scale-slider"
+                    type="range"
+                    min="0.5"
+                    max="10"
+                    step="0.1"
+                    value={state.scale}
+                    onChange={(e) => state.handleScaleChange(e, 10)}
+                  />
                 </div>
                 <TextField
                   id="iter"
@@ -74,21 +132,22 @@ const Deconvolution = () => {
                 <ChooseList
                   className="choose-list"
                   name="Deconvolution method"
-                  list={Object.values(state.deconvMethods)}
+                  list={Object.keys(state.deconvMethods)}
                   selected={state.deconvMethod}
                   onChange={state.handleDeconvMethodChange}
                 />
                 <Button
                   variant="outlined"
                   className="btn-run"
-                  disabled={!state.psfFiles.length} 
+                  disabled={!(state.sourceImage.length == 0 || state.extractedPSF.length)} 
+                  onClick={handleDeconvolve}
                 >
                   Deconvolve
                 </Button>
               </div>
               <div className="column-2" style={{ zIndex: 1 }}>
                 <div className="images__preview">
-                <TifCompare files_1={state.extractBeads} files_2={state.psfFiles} scale={state.scale} state={state} canvasRef={null} isExtract={false}/>
+                <TifCompare img_1={state.sourceImage} img_2={state.resultImage} scale={state.scale} state={state}/>
                 </div>
               </div>
             </div>
@@ -101,7 +160,15 @@ const Deconvolution = () => {
               <div className="column-1">
                 <div className="slider-container">
                   <label htmlFor="scale-slider">Scale:</label>
-                  <input id="scale-slider" type="range" min="0.5" max="10" step="0.1" value={state.scale} onChange={state.handleSliderChange} />
+                  <input
+                    id="scale-slider"
+                    type="range"
+                    min="0.5"
+                    max="10"
+                    step="0.1"
+                    value={state.scale}
+                    onChange={(e) => state.handleScaleChange(e, 10)}
+                  />
                 </div>
                 <TextField
                   id="filename"
@@ -114,10 +181,11 @@ const Deconvolution = () => {
                   onChange={(e) => state.setFilename(e.target.value)}
                   value={state.filename}
                 />
+                <FileDownloader fileList={state.resultImageSave} folderName={state.filename} btnName={"Save result"} />
               </div>
               <div className="column-2" style={{ zIndex: 1 }}>
                 <div className="images__preview">
-                  <TiffStackViewer tiffList={state.averageBead} scale={state.scale} state={state} canvasRef={null} isExtract={false}/>
+                  <TiffStackViewer tiffList={state.resultImage} scale={state.scale} state={state} canvasRef={null} isExtract={false}/>
                 </div>
               </div>
             </div>
