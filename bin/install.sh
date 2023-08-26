ddde3214c980cc222ff56c50fc9bbbddc8bcea4c
IMZolin <zolin5269@gmail.com>
2023-08-26 20:34:06 +0300

    Updated install.sh ch.8

--- bin/install.sh
+++ bin/install.sh
@@ -63,7 +63,7 @@ fi
 cd backend/engine
 git clone -b develop https://github.com/gerasimenkoab/simple_psf_extractor.git engine_lib
 cd engine_lib
-git pull oriin develop
+git pull origin develop
 cd ../../..
 
 # Install frontend dependencies
