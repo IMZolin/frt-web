a45cb97c4db6b1af7c8a14af2a3ffed6ab7ea885
ivanzolin <zolin.work@yandex.ru>
2023-11-27 18:31:44 +0300

    Updated allowed_hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-4t(z5^=)w7r&qg9d8&ow!99x#780y(ynjj()67g%%^snu7aubz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "localhost", "192.168.1.43"]
 
 
 # Application definition
