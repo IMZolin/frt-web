bbc6760e3cc674fbd72bd4e5ccfb2271792aa16c
Ilsaf <ilsaf.corp@gmail.com>
2023-11-27 12:13:01 +0000

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,10 +57,7 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True  
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-]
+CORS_ALLOWED_ORIGINS = "*"
 
 ROOT_URLCONF = 'backend.urls'
 
